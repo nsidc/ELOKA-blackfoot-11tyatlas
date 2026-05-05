a473b0317d7adf12e00d4dd41528df7bf4a27160
Brendan Billingsley <billingb@gmail.com>
2026-05-05 07:44:34 -0600

    Fix links bug

--- src/js/main.js
+++ src/js/main.js
@@ -146,7 +146,7 @@ const displayFeatureInfo = async function (id, props) {
     if (types.includes('demo_archive')) {
       const imageLinks = await Promise.all(
         relatedRecordsByType['demo_archive'].map(async (r) => {
-          const lids = links[id].map(l => l.tid)
+          const lids = links[r.tid].map(l => l.tid)
           const insertHtmlResponse = await fetch(`media/${r.tid}_insert.html`)
           const insertHtml = await insertHtmlResponse.text()
           return `<li x-data @mouseenter="$dispatch('hover', '${lids}')" @mouseleave="$dispatch('unhover')" class="list-row m-auto">${insertHtml}</li>`
